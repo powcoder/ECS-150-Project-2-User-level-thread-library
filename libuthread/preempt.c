2e7990b2825128cb10e7aa4ba5de9530d29bb772
powcoder <powcoder@163.com>
2022-11-28 11:23:53 +0800

    ECS 150 Project 2 User level thread library

---
+++ libuthread/preempt.c
@@ -0,0 +1,45 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#include <signal.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "private.h"
+#include "uthread.h"
+
+/*
+ * Frequency of preemption
+ * 100Hz is 100 times per second
+ */
+#define HZ 100
+
+void preempt_start(void)
+{
+	/* TODO */
+}
+
+void preempt_stop(void)
+{
+	/* TODO */
+}
+
+void preempt_enable(void)
+{
+	/* TODO */
+}
+
+void preempt_disable(void)
+{
+	/* TODO */
+}
+
